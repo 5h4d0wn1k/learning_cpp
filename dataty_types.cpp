c4b40571754a6d31e867ce33aa5c3c40887cddc9
5h4d0wn1k <nikhilnagpure203@gmail.com>
2023-07-05 21:25:37 +0530

    testing the work

--- dataty_types.cpp
+++ dataty_types.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
 using namespace std;
-int main () {
-    int x;//declaration 
-    x = 69;//assignment
-    //int datatype , size = 4 bytes 
+int main()
+{
+    int x;  // declaration
+    x = 69; // assignment
+    // int datatype , size = 4 bytes
     int a = 56;
     int age = 18;
-    //Double data type , size = 8 bytes , Stores fractional numbers
+    // Double data type , size = 8 bytes , Stores fractional numbers
     double c = 10.56;
     double height = 5.11;
-    //Char used for single character , size = 2 bytes
+    // Char used for single character , size = 2 bytes
     char grades = 'A';
     char character = 'A';
-    //boolean only contains (true or false), size = 1 bit
-    bool b1 = true ; 
-    bool student_attendance = true ;
-    //string ( used to types sequence of texts or words and lines)
+    // boolean only contains (true or false), size = 1 bit
+    bool b1 = true;
+    bool student_attendance = true;
+    // string ( used to types sequence of texts or words and lines)
     string Name = "Nikhil Nagpure ";
     string Bottle_colour = " Red ";
 
-    cout<<"Hello "<<Name<<
-    "you are "<<age<<
-    "year old \nyour height is "<<height<<
-    "\nYour bottle colour is "<<Bottle_colour<<
-    "\nYour attendence "<<student_attendance<<endl;
+    cout << "Hello " << Name << "you are " << age << "year old \nyour height is " << height << "\nYour bottle colour is " << Bottle_colour << "\nYour attendence " << student_attendance << endl;
     return 0;
 }
